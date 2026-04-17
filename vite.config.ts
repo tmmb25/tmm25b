dcca0b4baac2fd209bfa64a96520ccee6f82d3b5
tmmb25 <classtmm.b@gmail.com>
2026-04-18 06:30:51 +0700

    Fix base comma error

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tmm25b/"
+  base: "/tmm25b/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
